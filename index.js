b0823c4aae1f1df8c523d85a2ed65290cc54e536
Ayanda Sangweni <asangweni51@gmail.com>
2024-08-02 14:25:15 +0200

    innerhtml to append li to ul

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
     push(shoppingListInDB, inputValue)
-    console.log(inputValue)
+
+    inputFieldEl.value = ""
+    
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    
 })
